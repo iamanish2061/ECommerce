e5249befb1f684de139ea0afc2c5ed74926af88e
AnishShrestha <anishnitro5@gmail.com>
2025-11-26 11:58:32 +0545

    redis service added

---
+++ src/main/java/com/ECommerce/redis/RedisService.java
@@ -0,0 +1,43 @@
+package com.ECommerce.redis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class RedisService {
+
+    private static final Logger log = LoggerFactory.getLogger(RedisService.class);
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public String getCode(String key){
+        try{
+            Object o = redisTemplate.opsForValue().get(key);
+            return (o!=null)? o.toString():null;
+        }catch (Exception e){
+            log.error("Exception ",e);
+            return null;
+        }
+    }
+
+    public boolean setCode(String key, String value, Long ttl){
+        try{
+            redisTemplate.opsForValue().set(key, value, ttl, TimeUnit.SECONDS);
+            return true;
+        }catch (Exception e){
+            log.error("Exception " ,e);
+            return false;
+        }
+    }
+
+    public void deleteCode(String email) {
+        redisTemplate.delete(email);
+    }
+}
+

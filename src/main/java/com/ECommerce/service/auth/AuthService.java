cfbf3ac8c7080d94af78376599c124d0a9ea89ae
AnishShrestha <anishnitro5@gmail.com>
2025-11-26 11:55:47 +0545

    auth service update

--- src/main/java/com/ECommerce/service/auth/AuthService.java
+++ src/main/java/com/ECommerce/service/auth/AuthService.java
@@ -1,8 +1,10 @@
-package com.ECommerce.service;
+package com.ECommerce.service.auth;
 
 import com.ECommerce.model.UserPrincipal;
 import com.ECommerce.model.Users;
+import com.ECommerce.redis.RedisService;
 import com.ECommerce.repository.UserRepository;
+import com.ECommerce.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,9 +14,15 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @Service
 public class AuthService {
+    @Autowired
+    private RedisService redisService;
+
+    @Autowired
+    private EmailService emailService;
 
     @Autowired
     private UserRepository userRepo;
@@ -27,10 +35,8 @@ public class AuthService {
 
     private BCryptPasswordEncoder encoder= new BCryptPasswordEncoder(12);
 
-    //user
-    public Users register(Users user){
 
-        //validation
+    public Users register(Users user){
 
         if(userRepo.findByUsername(user.getUsername()).orElse(null) != null)
             throw new RuntimeException("Username already exists!!");
@@ -38,27 +44,25 @@ public class AuthService {
         if(userRepo.findByEmail(user.getUsername()).orElse(null) != null)
             throw new RuntimeException("Email already exists!!");
 
-        //verify code and email
-        // remove code from redis or wherever it is saved
+        if(!verifyCode(user.getEmail(), user.getCode()))
+            throw new RuntimeException("Email and code mismatched");
+        redisService.deleteCode(user.getEmail());
         user.setPassword(encoder.encode(user.getPassword()));
+
+        //baki xa sab set garna
         return userRepo.save(user);
     }
 
     public Map<String, String> login(Users user){
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
-        // authenticate is the unimplemented method of AuthenticationManager interface whose one of the implementations is done is ProviderManager class
-        // provider manager.authenticate() is called -> then it iterates through the available provider
-        // i am using DAO so its dynamically added to the list of providerManager
-        //          now authentication is done by dao class's object
-        //        in that method :the flow is
-        //        1. load the user by username from database
-        //        (method present in UserDetailsService) and i have implemented that on CustomUserDetailsService
-        //        2. verifying password by encoding entered one to db's password
-        //        3. returning an authenticated token with authorities
 
-
-        if (!authentication.isAuthenticated()) throw new RuntimeException("Invalid credentials");
+        if (!authentication.isAuthenticated()){
+            Map<String,String> result =  new HashMap<>();
+            result.put("status","failed");
+            result.put("message", "Invalid Credentials!");
+            return result;
+        }
 
         Users dbUser = userRepo.findByUsername(user.getUsername()).orElse(null);
 
@@ -92,7 +96,26 @@ public class AuthService {
         return tokens;
     }
 
+
     public boolean doesUserNameExist(String username) {
-        return (userRepo.findByUsername(username).orElse(null) == null) ? false: true;
+        return userRepo.existsByUsername(username);
+    }
+
+    public boolean doesEmailExist(String email) {
+        return userRepo.existsByEmail(email);
+    }
+
+    public boolean sendCode(String email) {
+        Integer otpCode = new Random().nextInt(900000) +100000;
+
+        redisService.setCode(email, otpCode.toString(), 600L);
+        String subject = "Verifying Email";
+        String emailBody = "Your otp code is: "+otpCode+". Please do not share with anyone! If you find this irrelevant, please ignore it!";
+        return emailService.sendEmail(new EmailService.EmailSender(email, subject,emailBody));
+    }
+
+    public boolean verifyCode(String email, String code) {
+        String generatedCode = redisService.getCode(email);
+        return generatedCode != null && generatedCode.equals(code);
     }
 }

fd9339ba046627d970fb6d62cf9fbe0ffc36912d
AnishShrestha <anishnitro5@gmail.com>
2025-11-27 21:05:42 +0545

    updated customer user details

--- src/main/java/com/ECommerce/service/CustomUserDetailsService.java
+++ src/main/java/com/ECommerce/service/CustomUserDetailsService.java
@@ -3,22 +3,21 @@ package com.ECommerce.service;
 import com.ECommerce.model.UserPrincipal;
 import com.ECommerce.model.Users;
 import com.ECommerce.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepo;
+    private final UserRepository userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Users user = userRepo.findByUsername(username).orElse(null);
-
 
         if(user == null){
             throw new UsernameNotFoundException("User not found!");

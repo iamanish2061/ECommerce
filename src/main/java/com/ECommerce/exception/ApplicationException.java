171491c7cd692c1ee575722e7aee2ec6695d7915
AnishShrestha <anishnitro5@gmail.com>
2025-11-27 21:08:12 +0545

    added custom exception

---
+++ src/main/java/com/ECommerce/exception/ApplicationException.java
@@ -0,0 +1,19 @@
+package com.ECommerce.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+public class ApplicationException extends Exception{
+
+    private final String errorCode;
+    private final HttpStatus status;
+
+    public ApplicationException(String message, String errorCode, HttpStatus status){
+        super(message);
+        this.errorCode = errorCode;
+        this.status = status;
+    }
+
+}

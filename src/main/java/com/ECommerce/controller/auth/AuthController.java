9dada9cc066dcc8bab0f0fbe09f76d4ea7f0a315
AnishShrestha <anishnitro5@gmail.com>
2025-11-28 11:52:04 +0545

    auth controller updated

--- src/main/java/com/ECommerce/controller/auth/AuthController.java
+++ src/main/java/com/ECommerce/controller/auth/AuthController.java
@@ -5,18 +5,21 @@ import com.ECommerce.dto.request.auth.RefreshTokenRequest;
 import com.ECommerce.dto.request.auth.SignupRequest;
 import com.ECommerce.dto.request.auth.VerifyOtpCodeRequest;
 import com.ECommerce.dto.response.ApiResponse;
-import com.ECommerce.dto.response.auth.LoginResponse;
+import com.ECommerce.dto.response.auth.AuthResponse;
 import com.ECommerce.dto.response.auth.RefreshTokenResponse;
-import com.ECommerce.dto.response.auth.SignupResponse;
 import com.ECommerce.exception.ApplicationException;
-import com.ECommerce.model.Users;
 import com.ECommerce.service.auth.AuthService;
+import com.ECommerce.utils.CookieUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.shaded.com.google.protobuf.Api;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,21 +29,21 @@ public class AuthController {
     private final AuthService authService;
 
     @GetMapping("/check-username-availability")
-    public ResponseEntity<ApiResponse> checkUserNameAvailability(
-            @RequestParam //validation
+    public ResponseEntity<ApiResponse<?>> checkUserNameAvailability(
+
+            @RequestParam
             String username
     ){
         if(authService.doesUserNameExist(username)){
             return ResponseEntity.status(HttpStatus.CONFLICT)
                     .body(ApiResponse.error("Username is taken.", "USERNAME_EXISTS"));
         }
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.ok("Username is available."));
+        return ResponseEntity.ok(ApiResponse.ok("Username is available."));
     }
 
     @GetMapping("/check-email-and-send-otpCode")
-    public ResponseEntity<ApiResponse> sendOtpCode(
-            @RequestBody //validation
+    public ResponseEntity<ApiResponse<?>> sendOtpCode(
+            @RequestBody
             String email
     ){
         if(authService.doesEmailExist(email)){
@@ -51,15 +54,13 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
                     body(ApiResponse.error("Failed to send Code", "FAILED_TO_SEND_CODE"));
         }
-        return ResponseEntity.status(HttpStatus.OK).
-                body(ApiResponse.ok("Code sent successfully"));
+        return ResponseEntity.ok(ApiResponse.ok("Code sent successfully"));
     }
 
     @GetMapping("/verify-otpCode")
-    public ResponseEntity<ApiResponse> verifyOtpCode(@RequestBody VerifyOtpCodeRequest request){
+    public ResponseEntity<ApiResponse<?>> verifyOtpCode(@RequestBody VerifyOtpCodeRequest request){
         if(authService.verifyOtpCode(request.email(), request.code())){
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(ApiResponse.ok("OTP verified."));
+            return ResponseEntity.ok(ApiResponse.ok("OTP verified."));
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ApiResponse.error("Invalid OTP code.", "INVALID_CODE"));
@@ -69,19 +70,34 @@ public class AuthController {
 
 
     @PostMapping("/register")
-    public ResponseEntity<SignupResponse> register(@RequestBody SignupRequest request) throws ApplicationException {
-        return ResponseEntity.ok(authService.register(request));
+    public ResponseEntity<ApiResponse<AuthResponse>> register(
+            @Valid @RequestBody SignupRequest request, HttpServletResponse httpResponse) throws ApplicationException {
+        AuthResponse authResponse = authService.register(request, httpResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.ok(authResponse, "Account created successfully"));
     }
 
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok(authService.login(request));
+    public ResponseEntity<ApiResponse<AuthResponse>> login(
+            @RequestBody LoginRequest request, HttpServletResponse httpResponse) {
+        AuthResponse authResponse = authService.login(request, httpResponse);
+        return ResponseEntity.ok(ApiResponse.ok(authResponse, "Logged in successfully!"));
     }
 
-    @PostMapping("/refresh")
-    public ResponseEntity<RefreshTokenResponse> refresh(@RequestBody RefreshTokenRequest request) {
-        return ResponseEntity.ok(authService.refresh(request));
+    @PostMapping("/refresh-token")
+    public ResponseEntity<ApiResponse<AuthResponse>> refreshToken(
+            @RequestBody HttpServletRequest request) {
+        AuthResponse authResponse = authService.refreshToken(request);
+        return ResponseEntity.ok(
+                ApiResponse.ok(authResponse, "Token refreshed successfully!")
+        );
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<ApiResponse<?>> logout(HttpServletResponse response, Authentication authentication) {
+        authService.logout(authentication, response);
+        return ResponseEntity.ok(
+                ApiResponse.ok("Logged out successfully"));
     }
 
 }

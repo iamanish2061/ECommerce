53b94e9049ae5d029829ce5b70488d5e76719c96
AnishShrestha <anishnitro5@gmail.com>
2025-11-28 12:00:17 +0545

    Added validation

--- src/main/java/com/ECommerce/dto/request/auth/LoginRequest.java
+++ src/main/java/com/ECommerce/dto/request/auth/LoginRequest.java
@@ -1,10 +1,17 @@
 package com.ECommerce.dto.request.auth;
 
 import com.ECommerce.model.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
 
+        @NotBlank(message = "Username is required!")
+        @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters!")
+        @Pattern(regexp = "^[A-Za-z0-9_]+$", message = "Username can only contain letters, numbers, and underscores!")
         String username,
+
 
         String password,
         Role role

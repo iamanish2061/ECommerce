aabec1646bd1cbdef03afce307fb5d83aec38afa
AnishShrestha <anishnitro5@gmail.com>
2025-11-28 12:00:44 +0545

    Added validation

--- src/main/java/com/ECommerce/dto/request/auth/RefreshTokenRequest.java
+++ src/main/java/com/ECommerce/dto/request/auth/RefreshTokenRequest.java
@@ -1,5 +1,8 @@
 package com.ECommerce.dto.request.auth;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record RefreshTokenRequest(
+        @NotBlank(message = "Refresh Token not found!")
         String refreshToken
 ) {}

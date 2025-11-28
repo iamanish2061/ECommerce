84adeb67771c4e335e047db5599277e73cf93458
AnishShrestha <anishnitro5@gmail.com>
2025-11-28 11:58:29 +0545

    Added validation

--- src/main/java/com/ECommerce/dto/request/auth/VerifyOtpCodeRequest.java
+++ src/main/java/com/ECommerce/dto/request/auth/VerifyOtpCodeRequest.java
@@ -1,9 +1,14 @@
 package com.ECommerce.dto.request.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
 public record VerifyOtpCodeRequest(
 
+        @Email(message = "Invalid Email")
         String email,
 
+        @Size(min = 6, max = 6, message = "Invalid Code")
         String code
 ) {
 }

7af6c6dc4aefcdef8b2842f520d7a75013679d90
AnishShrestha <anishnitro5@gmail.com>
2025-11-28 11:56:49 +0545

    Cleaned

--- src/main/java/com/ECommerce/dto/request/auth/SignupRequest.java
+++ src/main/java/com/ECommerce/dto/request/auth/SignupRequest.java
@@ -1,7 +1,9 @@
 package com.ECommerce.dto.request.auth;
 
-import com.ECommerce.model.Role;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record SignupRequest(
 

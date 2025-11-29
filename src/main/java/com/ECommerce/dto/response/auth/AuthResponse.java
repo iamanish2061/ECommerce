d958dd0a0cc56db4ebf739b10d8d310a478b1061
AnishShrestha <anishnitro5@gmail.com>
2025-11-29 19:38:30 +0545

    managed

--- src/main/java/com/ECommerce/dto/response/auth/AuthResponse.java
+++ src/main/java/com/ECommerce/dto/response/auth/AuthResponse.java
@@ -1,24 +1,19 @@
 package com.ECommerce.dto.response.auth;
 
+import com.ECommerce.model.Role;
+
 public record AuthResponse(
-
         String accessToken,
-
         String tokenType,
-
         Long expiresIn,
-
         Long userId,
-
         String fullName,
-
         String username,
-
-        String email
-
+        String email,
+        Role role
 ) {
     public AuthResponse(String accessToken, Long userId, String fullName,
-                        String username, String email) {
-        this(accessToken, "Bearer", 1800L, userId, fullName, username, email);
+                        String username, String email, Role role) {
+        this(accessToken, "Bearer", 1800L, userId, fullName, username, email, role);
     }
 }

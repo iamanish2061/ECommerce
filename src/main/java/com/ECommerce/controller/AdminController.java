aa0503ab9eacb70bc8972881f30f4760b729bbe4
AnishShrestha <anishnitro5@gmail.com>
2025-11-29 20:47:04 +0545

    added method

--- src/main/java/com/ECommerce/controller/AdminController.java
+++ src/main/java/com/ECommerce/controller/AdminController.java
@@ -3,12 +3,15 @@ package com.ECommerce.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class AdminController {
 
     @GetMapping("/get-users")
     public List<AdminUserRequest> getUsers(){
 
+        return new ArrayList<>();
     }
 
 }

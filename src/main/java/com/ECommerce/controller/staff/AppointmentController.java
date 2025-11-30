652944c397bdc226a580fc1d3d8050d9d8a2663f
AnishShrestha <anishnitro5@gmail.com>
2025-11-30 14:43:32 +0545

    getting started

---
+++ src/main/java/com/ECommerce/controller/staff/AppointmentController.java
@@ -0,0 +1,9 @@
+package com.ECommerce.controller.staff;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/staff")
+public class AppointmentController {
+}

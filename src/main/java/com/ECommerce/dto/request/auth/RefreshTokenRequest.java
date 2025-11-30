0b7d555f3004849c9fce7df99fe872b83366edb1
AnishShrestha <anishnitro5@gmail.com>
2025-11-30 14:40:00 +0545

    deleted

--- src/main/java/com/ECommerce/dto/request/auth/RefreshTokenRequest.java
+++
@@ -1,8 +0,0 @@
-package com.ECommerce.dto.request.auth;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RefreshTokenRequest(
-        @NotBlank(message = "Refresh Token not found!")
-        String refreshToken
-) {}
--- src/main/java/com/ECommerce/dto/response/auth/RefreshTokenResponse.java
+++
@@ -1,4 +0,0 @@
-package com.ECommerce.dto.response.auth;
-
-public record RefreshTokenResponse {
-}

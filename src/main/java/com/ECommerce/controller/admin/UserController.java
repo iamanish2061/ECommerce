d58bbd6381dcb492d24a42fee821c12cf4f72d2d
AnishShrestha <anishnitro5@gmail.com>
2025-11-30 19:15:06 +0545

    updated

--- src/main/java/com/ECommerce/controller/admin/UserController.java
+++ src/main/java/com/ECommerce/controller/admin/UserController.java
@@ -5,15 +5,13 @@ import com.ECommerce.dto.response.admin.GetAllUserResponse;
 import com.ECommerce.dto.response.admin.SingleUserDetailResponse;
 import com.ECommerce.exception.ApplicationException;
 import com.ECommerce.service.admin.UserService;
+import com.ECommerce.validation.ValidId;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,21 +23,19 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/get-all-users")
+    @GetMapping("/users")
     public ResponseEntity<ApiResponse<List<GetAllUserResponse>>> getAllUsers(){
+        //pagination
         List<GetAllUserResponse> users = userService.getAllUsers();
         return ResponseEntity.ok(ApiResponse.ok(users, "Fetched info of all users"));
     }
 
-    @GetMapping("/get-details-single-user")
+    @GetMapping("/user/{id}")
     public ResponseEntity<ApiResponse<SingleUserDetailResponse>> getSingleUser(
-            @RequestParam("id")
-            @NotNull(message = "User ID is required")
-            @Positive(message = "User ID must be a positive number")
-            Long id
+            @ValidId @PathVariable Long id
     )throws ApplicationException {
         SingleUserDetailResponse response = userService.getSingleUserInfo(id);
-        return ResponseEntity.ok(ApiResponse.ok(response, "Information of user fetched successfully"));
+        return ResponseEntity.ok(ApiResponse.ok(response, "User detail fetched successfully"));
     }
 
 

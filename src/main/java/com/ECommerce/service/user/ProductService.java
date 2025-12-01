c787a9ddd5ee2fdec118e1f8eeb325705107f206
AnishShrestha <anishnitro5@gmail.com>
2025-12-01 18:05:53 +0545

    product related logics

--- src/main/java/com/ECommerce/service/user/ProductService.java
+++ src/main/java/com/ECommerce/service/user/ProductService.java
@@ -1,7 +1,22 @@
 package com.ECommerce.service.user;
 
+import com.ECommerce.dto.response.admin.AllProductsResponse;
+import com.ECommerce.dto.response.admin.SingleProductResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+
+
+
+    public List<AllProductsResponse> getAllProducts() {
+
+    }
+
+
+    public SingleProductResponse getDetailOfProduct(Long id) {
+
+    }
 }

b15d5ea1535d408be8b157ffec873a6d5f7841e3
AnishShrestha <anishnitro5@gmail.com>
2025-12-01 18:04:25 +0545

    updated otp code's work

--- src/main/java/com/ECommerce/service/admin/UserService.java
+++ src/main/java/com/ECommerce/service/admin/UserService.java
@@ -3,13 +3,14 @@ package com.ECommerce.service.admin;
 import com.ECommerce.dto.response.admin.GetAllUserResponse;
 import com.ECommerce.dto.response.admin.SingleUserDetailResponse;
 import com.ECommerce.exception.ApplicationException;
+import com.ECommerce.model.Role;
+import com.ECommerce.model.UserStatus;
 import com.ECommerce.model.Users;
 import com.ECommerce.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -46,6 +47,21 @@ public class UserService {
         );
     }
 
+    public boolean updateRole(Long id, Role role) throws ApplicationException{
+        Users user = userRepository.findById(id).orElse(null);
+        if(user == null)
+            throw new ApplicationException("User not found!", "USER_NOT_FOUND", HttpStatus.BAD_REQUEST);
+        user.setRole(role);
+        userRepository.save(user);
+        return true;
+    }
 
-
+    public boolean updateStatus(Long id, UserStatus status) throws ApplicationException{
+        Users user = userRepository.findById(id).orElse(null);
+        if(user == null)
+            throw new ApplicationException("User not found!", "USER_NOT_FOUND", HttpStatus.BAD_REQUEST);
+        user.setStatus(status);
+        userRepository.save(user);
+        return true;
+    }
 }

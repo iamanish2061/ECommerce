8bbe412b2be7f17cb7b14b53067e5adaf2ec0274
AnishShrestha <anishnitro5@gmail.com>
2025-12-01 18:05:35 +0545

    product related end points for user

--- src/main/java/com/ECommerce/controller/user/ProductController.java
+++ src/main/java/com/ECommerce/controller/user/ProductController.java
@@ -1,9 +1,45 @@
 package com.ECommerce.controller.user;
 
+import com.ECommerce.dto.response.ApiResponse;
+import com.ECommerce.dto.response.admin.AllProductsResponse;
+import com.ECommerce.dto.response.admin.SingleProductResponse;
+import com.ECommerce.service.user.ProductService;
+import com.ECommerce.validation.ValidId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Validated
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/user/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping()
+    public ResponseEntity<ApiResponse<List<AllProductsResponse>>> getAllProducts(){
+        List<AllProductsResponse> products = productService.getAllProducts();
+        return ResponseEntity.ok(ApiResponse.ok(products, "Fetched successfully"));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<SingleProductResponse>> getDetailOfProduct(
+            @ValidId @PathVariable Long id
+    ){
+        SingleProductResponse product = productService.getDetailOfProduct(id);
+        return ResponseEntity.ok(ApiResponse.ok(product, "Fetched successfully"));
+    }
+
+    //searched product
+
+    //category
+
+
 }

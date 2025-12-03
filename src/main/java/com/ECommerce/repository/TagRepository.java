1c20af5d045b1a33384f76b8e9c1a68bb39fa7a2
AnishShrestha <anishnitro5@gmail.com>
2025-12-03 21:17:48 +0545

    added slug method

--- src/main/java/com/ECommerce/repository/TagRepository.java
+++ src/main/java/com/ECommerce/repository/TagRepository.java
@@ -4,10 +4,15 @@ import com.ECommerce.model.product.TagModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface TagRepository extends JpaRepository<TagModel, Long> {
     Optional<TagModel> findBySlug(String slug);
 
+    Set<String> findSlugsBySlugsIn(List<String> incomingSlugs);
+
+    Optional<TagModel> findByName(String name);
 }

898cbbc0b33f0f7dd6f25043653839e96713ce38
AnishShrestha <anishnitro5@gmail.com>
2025-12-03 21:16:49 +0545

    working on managing product

--- src/main/java/com/ECommerce/controller/admin/AdminProductController.java
+++ src/main/java/com/ECommerce/controller/admin/AdminProductController.java
@@ -1,16 +1,18 @@
 package com.ECommerce.controller.admin;
 
-import com.ECommerce.dto.request.admin.AddProductRequest;
+import com.ECommerce.dto.request.product.*;
 import com.ECommerce.dto.response.ApiResponse;
-import com.ECommerce.dto.response.admin.product.AllProductsResponse;
-import com.ECommerce.dto.response.admin.product.SingleProductResponse;
+import com.ECommerce.dto.response.product.AllProductsResponse;
+import com.ECommerce.dto.response.product.SingleProductResponse;
 import com.ECommerce.exception.ApplicationException;
 import com.ECommerce.service.admin.AdminProductService;
 import com.ECommerce.service.user.ProductService;
 import com.ECommerce.validation.ValidId;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -27,10 +29,62 @@ public class AdminProductController {
     private final AdminProductService adminProductService;
     private final ProductService productService;
 
-    @PostMapping("/")
+
+    @PostMapping("/tags")
+    public ResponseEntity<ApiResponse<String>> addTags(
+            @Valid @RequestBody AddTagRequest tagRequests
+    )throws ApplicationException{
+        if(adminProductService.addTags(tagRequests))
+            return ResponseEntity.ok(ApiResponse.ok("Tags added successfully"));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("Failed to add!", "FAILED_TO_ADD"));
+    }
+
+    @PutMapping("/tags")
+    public ResponseEntity<ApiResponse<String>> deleteTag(
+        @Pattern(regexp = "^[a-zA-Z0-9\\s&()\\-.,:]{2,100}$", message ="Invalid Tag!")
+        @RequestParam String name
+    ){
+        if(adminProductService.deleteTag(name))
+            return ResponseEntity.ok(ApiResponse.ok("Tags deleted successfully"));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("Failed to delete!", "FAILED_TO_ADD"));
+
+    }
+
+    @PostMapping(value = "/brand", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse<?>> addBrand(
+        @Valid @RequestPart("addBrandRequest") AddBrandRequest addBrandRequest,
+        @RequestPart("logo") MultipartFile logo
+    ){
+
+    }
+
+    @PostMapping(value = "/category", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse<?>> addCategory(
+        @Valid @RequestPart("addCategoryRequest")AddCategoryRequest addCategoryRequest,
+        @RequestPart("imageUrl") MultipartFile imageUrl
+    ){
+
+    }
+
+    @PostMapping(value = "/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse<?>> addImage(
+        @Valid @RequestPart("addProductImageRequest") AddProductImageRequest addProductImageRequest,
+        @RequestPart("url") MultipartFile url
+    ){
+
+    }
+
+
+
+
+
+
+    @PostMapping(value = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse<SingleProductResponse>> addNewProduct(
-            @Valid @RequestPart("addProductRequest") AddProductRequest addProductRequest,
-            @RequestPart("imageFiles") List<MultipartFile> imageFiles
+        @Valid @RequestPart("addProductRequest") AddProductRequest addProductRequest,
+        @RequestPart("imageFiles") List<MultipartFile> imageFiles
     )throws ApplicationException {
         SingleProductResponse response = adminProductService.addNewProduct(addProductRequest, imageFiles);
         return ResponseEntity.status(HttpStatus.CREATED)
--- src/main/java/com/ECommerce/service/admin/AdminProductService.java
+++ src/main/java/com/ECommerce/service/admin/AdminProductService.java
@@ -1,9 +1,14 @@
 package com.ECommerce.service.admin;
 
-import com.ECommerce.dto.request.admin.AddProductImageRequest;
-import com.ECommerce.dto.request.admin.AddProductRequest;
-import com.ECommerce.dto.response.admin.product.SingleProductResponse;
+import com.ECommerce.dto.request.product.AddProductImageRequest;
+import com.ECommerce.dto.request.product.AddProductRequest;
 
+import com.ECommerce.dto.request.product.AddTagRequest;
+import com.ECommerce.dto.response.product.BrandResponse;
+import com.ECommerce.dto.response.product.CategoryResponse;
+import com.ECommerce.dto.response.product.ProductImageResponse;
+import com.ECommerce.dto.response.product.TagResponse;
+import com.ECommerce.dto.response.product.SingleProductResponse;
 import com.ECommerce.exception.ApplicationException;
 import com.ECommerce.model.product.*;
 import com.ECommerce.repository.BrandRepository;
@@ -11,9 +16,12 @@ import com.ECommerce.repository.CategoryRepository;
 import com.ECommerce.repository.ProductRepository;
 import com.ECommerce.repository.TagRepository;
 import com.ECommerce.utils.HelperClass;
+import com.ECommerce.utils.ImageUploadHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
@@ -30,31 +38,57 @@ public class AdminProductService {
     private final TagRepository tagRepository;
     private final ProductRepository productRepository;
 
+    @Transactional
+    public boolean addTags(AddTagRequest request) throws ApplicationException {
+        List<String> incomingNames = request.names().stream()
+                .map(String::trim)
+                .toList();
+
+        List<String> incomingSlugs = incomingNames.stream()
+                .map(HelperClass::generateSlug)
+                .toList();
+
+        Set<String> existingSlugs = tagRepository.findSlugsBySlugsIn(incomingSlugs);
+
+        List<TagModel> newTagModels = new ArrayList<>();
+
+        for (String name : incomingNames) {
+            String slug = HelperClass.generateSlug(name);
+
+            if (!existingSlugs.contains(slug)) {
+                newTagModels.add(new TagModel(name, slug));
+            }
+        }
+
+        if (!newTagModels.isEmpty()) {
+            tagRepository.saveAll(newTagModels);
+        }
+        return true;
+    }
+
+    @Transactional
+    public boolean deleteTag(String name){
+        TagModel tag = tagRepository.findByName(name).orElseThrow(()->
+                new ApplicationException("Tag not found!", "INVALID_TAG", HttpStatus.BAD_REQUEST));
+
+        tagRepository.delete(tag);
+        return true;
+    }
+
+
+
+
+
+
+
+    @Transactional
     public SingleProductResponse addNewProduct(AddProductRequest request, List<MultipartFile> imageFiles) throws ApplicationException{
-
-        //code for saving images here
-
 
         BrandModel brand = brandRepository.findByName(request.brandName().trim())
                 .orElseThrow(()->new ApplicationException("Brand not found", "BRAND_NOT_FOUND", HttpStatus.BAD_REQUEST));
 
         CategoryModel category = categoryRepository.findByName(request.categoryName().trim())
                 .orElseThrow(()->new ApplicationException("Category not found!", "CATEGORY_NOT_FOUND", HttpStatus.BAD_REQUEST));
-
-        // Creating Product
-        ProductModel product = new ProductModel();
-        product.setSku(request.sku().trim().toUpperCase());
-        product.setTitle(request.title().trim());
-        product.setSlug(HelperClass.generateSlug(request.title().trim()));
-        product.setShortDescription(request.shortDescription());
-        product.setDescription(request.description());
-        product.setBrand(brand);
-        product.setCategory(category);
-        product.setBasePrice(request.basePrice());
-        product.setSellingPrice(request.sellingPrice());
-        product.setStock(request.stock() != null ? request.stock() : 0);
-        product.setSizeMl(request.sizeMl());
-        product.setActive(request.active());
 
         // Handling Tags
         Set<TagModel> tags = new HashSet<>();
@@ -65,26 +99,71 @@ public class AdminProductService {
                     .orElseThrow(()-> new ApplicationException("Tag not found!", "TAG_NOT_FOUND", HttpStatus.BAD_REQUEST));
             tags.add(tag);
         }
+
+        // Creating Product
+        ProductModel product = new ProductModel();
+        product.setSku(request.sku().trim().toUpperCase());
+        product.setTitle(request.title().trim());
+        product.setSlug(HelperClass.generateSlug(request.title().trim()));
+        product.setShortDescription(request.shortDescription().trim());
+        product.setDescription(request.description().trim());
+        product.setBrand(brand);
+        product.setCategory(category);
+        product.setBasePrice(request.basePrice());
+        product.setSellingPrice(request.sellingPrice());
+        product.setStock(request.stock() != null ? request.stock() : 0);
+        product.setSizeMl(request.sizeMl());
+        product.setActive(request.active());
         product.setTags(tags);
 
-        // Handling Images (which are already uploaded & saved permanently)
-        List<ProductImageModel> images = new ArrayList<>();
-        for (int i = 0; i < request.images().size(); i++) {
-            AddProductImageRequest imgReq = request.images().get(i);
 
-            ProductImageModel img = new ProductImageModel();
-            img.setUrl(imgReq.name());           // this is the final URL: /uploads/products/xxx.jpg
-            img.setAltText(imgReq.altText() != null ? imgReq.altText() : request.title());
-            img.setSortOrder(imgReq.sortOrder());
-            img.setThumbnail(imgReq.thumbnail());
-            img.setProduct(product);             // back reference
-            images.add(img);
+        List<ProductImageModel> imageModels = new ArrayList<>();
+
+        if (imageFiles != null && !imageFiles.isEmpty()) {
+            for (int i = 0; i < imageFiles.size(); i++) {
+                MultipartFile file = imageFiles.get(i);
+                AddProductImageRequest imgReq = request.images().get(i);
+
+                ProductImageModel imageModel = ImageUploadHelper.uploadImage(file, imgReq);
+                imageModel.setProduct(product);
+                imageModels.add(imageModel);
+            }
         }
-        product.setImages(images);
+        product.setImages(imageModels);
+
 
         // 6. Save everything (cascade handles images + tags)
         ProductModel savedProduct = productRepository.save(product);
-        return null;
+
+
+        return new SingleProductResponse(
+                savedProduct.getId(),
+                savedProduct.getSku(),
+                savedProduct.getTitle(),
+                savedProduct.getShortDescription(),
+                savedProduct.getDescription(),
+                new BrandResponse(
+                        savedProduct.getBrand().getName(),
+                        savedProduct.getBrand().getSlug(),
+                        savedProduct.getBrand().getLogoUrl()
+                ),
+                new CategoryResponse(
+                        savedProduct.getCategory().getName(),
+                        savedProduct.getCategory().getSlug(),
+                        savedProduct.getCategory().getImageUrl()
+                ),
+                savedProduct.getSellingPrice(),
+                savedProduct.getStock(),
+                savedProduct.getSizeMl(),
+                savedProduct.getTags()
+                        .stream()
+                        .map(tag-> new TagResponse(tag.getName(), tag.getSlug()))
+                        .toList(),
+                savedProduct.getImages()
+                        .stream()
+                        .map(img-> new ProductImageResponse(img.getUrl(), img.getAltText(), img.isThumbnail()))
+                        .toList()
+                );
     }
 
     public boolean updatePrice(Long id, Double price) {

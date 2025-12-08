e61c44105cd3fa6e11c45c36b03b177846e1740a
AnishShrestha <anishnitro5@gmail.com>
2025-12-08 14:07:26 +0545

    added jpql for fetching brands and related info

--- src/main/java/com/ECommerce/repository/product/ProductRepository.java
+++ src/main/java/com/ECommerce/repository/product/ProductRepository.java
@@ -1,12 +1,32 @@
 package com.ECommerce.repository.product;
 
+import com.ECommerce.dto.projection.BrandWithProductProjection;
 import com.ECommerce.model.product.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductModel, Long> {
+
+    @Query("""
+    SELECT
+        b.name, b.slug, b.logoUrl,
+        p.id, p.title, p.shortDescription, p.sellingPrice, p.stock,
+        (SELECT pi.url FROM ProductImageModel pi
+         WHERE pi.product = p AND pi.thumbnail = true
+         ORDER BY pi.sortOrder ASC, pi.id ASC LIMIT 1) AS imageUrl
+    FROM ProductModel p
+    JOIN p.brand b
+    WHERE b.slug = :brandSlug AND p.active = true
+    ORDER BY p.id
+    """)
+    List<Object[]> findAllByBrandSlug(@Param("brandSlug") String brandSlug);
+
+
+
     List<ProductModel> findByIdNotIn(List<Long> personalizedProductIds);
 }

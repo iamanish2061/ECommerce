1ee035aaf16dd63f547bf51172921239d7dad7f0
AnishShrestha <anishnitro5@gmail.com>
2025-12-10 17:01:26 +0545

    updated order item model

--- src/main/java/com/ECommerce/model/cartandorders/OrderItem.java
+++ src/main/java/com/ECommerce/model/cartandorders/OrderItem.java
@@ -4,22 +4,16 @@ import com.ECommerce.model.product.ProductModel;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
+
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "order_items")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class OrderItem {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "order_id", nullable = false)
-    private Long orderId;
-
-    @Column(name = "product_id", nullable = false)
-    private Long productId;
 
     @Column(nullable = false)
     private int quantity;
@@ -28,11 +22,11 @@ public class OrderItem {
     private BigDecimal priceAtPurchase;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id", insertable = false, updatable = false)
+    @JoinColumn(name = "order_id", nullable = false)
     @JsonIgnoreProperties("orderItems")
     private OrderModel order;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", insertable = false, updatable = false)
+    @JoinColumn(name = "product_id", nullable = false)
     private ProductModel product;
 }
